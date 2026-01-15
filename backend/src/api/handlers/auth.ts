75fae22489c2ea11a90bc967096678e3dae2853b
dufei <dufei@dufeideMacBook-Pro.local>
2026-01-15 22:08:26 +0800

    您的提交信息

--- backend/src/api/handlers/auth.ts
+++ backend/src/api/handlers/auth.ts
@@ -73,7 +73,7 @@ router.post('/register', async (c) => {
     const result = await authService.register(body);
 
     if (result.isErr()) {
-      throw new HttpExceptions.BadRequestException(`${result.error}`, result.error);
+      return new HttpExceptions.BadRequestException(`Bad Request`, result.error).getResponse();
     }
 
     return c.json(new HttpExceptions.SuccessResponse(result.value), 201);
